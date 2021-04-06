9801975d9623ca3ffbca3147113ab4b2567c6c76
fikrimus35 <mustofa13.fikri@gmail.com>
2021-04-06 07:07:21 +0700

    Fix CORS problem
    Signed-off-by: fikrimus35 <mustofa13.fikri@gmail.com>

--- src/api/pagubris.js
+++ src/api/pagubris.js
@@ -1,10 +1,5 @@
 import axios from "axios";
-import { getToken } from "../actions/userActions";
 
 export default axios.create({
   baseURL: "https://api.pagubris.my.id/v1/",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
 });
